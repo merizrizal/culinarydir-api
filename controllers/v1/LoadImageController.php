5a80143744dc5f8d5f043a5af151201c9d721400
AxelRj13 <axel.raharja1@gmail.com>
2019-03-29 13:21:42 +0700

    Penyederhanaan code

--- controllers/v1/LoadImageController.php
+++ controllers/v1/LoadImageController.php
@@ -22,11 +22,7 @@ class LoadImageController extends \yii\web\Controller {
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-                        'registry-business' => ['GET'],
-                        'user' => ['GET'],
-                        'user-post' => ['GET'],
-                        'business-promo' => ['GET'],
-                        'promo' => ['GET'],
+                        'load-image' => ['GET'],
                     ],
                 ],
             ]);
@@ -34,141 +30,38 @@ class LoadImageController extends \yii\web\Controller {
     
     public function actionRegistryBusiness($image, $w = null, $h = null)
     {
-        $this->image = Yii::getAlias('@uploads') . '/img/registry_business/' . $image;
-        
-        if (!empty($w) || !empty($h)) {
-            
-            $this->image = Yii::getAlias('@uploads') . '/img/registry_business/'  . $w . 'x' . $h . $image;
-            
-            Image::thumbnail('@uploads' . '/img/registry_business/' . $image, $w, $h)
-                ->save($this->image);
-        }
-        
-        Yii::$app->getResponse()->getHeaders()
-            ->set('Pragma', 'public')
-            ->set('Expires', '0')
-            ->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
-            ->set('Content-Transfer-Encoding', 'binary')
-            ->set('Content-type', 'image/jpeg');
-        
-        try {
-            
-            Yii::$app->response->stream = fopen($this->image, 'r');
-        } catch (\yii\base\ErrorException $e) {
-
-            throw new NotFoundHttpException('The requested image does not exist.');
-        }
-        
-        Yii::$app->response->format = Response::FORMAT_RAW;
-        
-        return Yii::$app->response->send();
+        return $this->loadImage('registry_business', $image, $w, $h);
     }
     
     public function actionUser($image, $w = null, $h = null)
     {
-        $this->image = Yii::getAlias('@uploads') . '/img/user/' . $image;
-        
-        if (!empty($w) || !empty($h)) {
-            
-            $this->image = Yii::getAlias('@uploads') . '/img/user/'  . $w . 'x' . $h . $image;
-            
-            Image::thumbnail('@uploads' . '/img/user/' . $image, $w, $h)
-                ->save($this->image);
-        }
-        
-        Yii::$app->getResponse()->getHeaders()
-            ->set('Pragma', 'public')
-            ->set('Expires', '0')
-            ->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
-            ->set('Content-Transfer-Encoding', 'binary')
-            ->set('Content-type', 'image/jpeg');
-        
-        try {
-            
-            Yii::$app->response->stream = fopen($this->image, 'r');
-        } catch (\yii\base\ErrorException $e) {
-            
-            throw new NotFoundHttpException('The requested image does not exist.');
-        }
-        
-        Yii::$app->response->format = Response::FORMAT_RAW;
-        
-        return Yii::$app->response->send();
+        return $this->loadImage('user', $image, $w, $h);
     }
     
     public function actionUserPost($image, $w = null, $h = null)
     {
-        $this->image = Yii::getAlias('@uploads') . '/img/user_post/' . $image;
-        
-        if (!empty($w) || !empty($h)) {
-            
-            $this->image = Yii::getAlias('@uploads') . '/img/user_post/'  . $w . 'x' . $h . $image;
-            
-            Image::thumbnail('@uploads' . '/img/user_post/' . $image, $w, $h)
-                ->save($this->image);
-        }
-        
-        Yii::$app->getResponse()->getHeaders()
-            ->set('Pragma', 'public')
-            ->set('Expires', '0')
-            ->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
-            ->set('Content-Transfer-Encoding', 'binary')
-            ->set('Content-type', 'image/jpeg');
-        
-        try {
-            
-            Yii::$app->response->stream = fopen($this->image, 'r');
-        } catch (\yii\base\ErrorException $e) {
-            
-            throw new NotFoundHttpException('The requested image does not exist.');
-        }
-        
-        Yii::$app->response->format = Response::FORMAT_RAW;
-        
-        return Yii::$app->response->send();
+        return $this->loadImage('user_post', $image, $w, $h);
     }
     
     public function actionBusinessPromo($image, $w = null, $h = null)
     {
-        $this->image = Yii::getAlias('@uploads') . '/img/business_promo/' . $image;
-        
-        if (!empty($w) || !empty($h)) {
-            
-            $this->image = Yii::getAlias('@uploads') . '/img/business_promo/'  . $w . 'x' . $h . $image;
-            
-            Image::thumbnail('@uploads' . '/img/business_promo/' . $image, $w, $h)
-                ->save($this->image);
-        }
-        
-        Yii::$app->getResponse()->getHeaders()
-            ->set('Pragma', 'public')
-            ->set('Expires', '0')
-            ->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
-            ->set('Content-Transfer-Encoding', 'binary')
-            ->set('Content-type', 'image/jpeg');
-        
-        try {
-            
-            Yii::$app->response->stream = fopen($this->image, 'r');
-        } catch (\yii\base\ErrorException $e) {
-            
-            throw new NotFoundHttpException('The requested image does not exist.');
-        }
-        
-        Yii::$app->response->format = Response::FORMAT_RAW;
-        
-        return Yii::$app->response->send();
+        return $this->loadImage('business_promo', $image, $w, $h);
     }
     
     public function actionPromo($image, $w = null, $h = null)
     {
-        $this->image = Yii::getAlias('@uploads') . '/img/promo/' . $image;
+        return $this->loadImage('promo', $image, $w, $h);
+    }
+    
+    private function loadImage($directory, $image, $w = null, $h = null)
+    {
+        $this->image = Yii::getAlias('@uploads') . '/img/' . $directory . '/' . $image;
         
         if (!empty($w) || !empty($h)) {
             
-            $this->image = Yii::getAlias('@uploads') . '/img/promo/'  . $w . 'x' . $h . $image;
+            $this->image = Yii::getAlias('@uploads') . '/img/' . $directory . '/'  . $w . 'x' . $h . $image;
             
-            Image::thumbnail('@uploads' . '/img/promo/' . $image, $w, $h)
+            Image::thumbnail('@uploads' . '/img/' . $directory . '/' . $image, $w, $h)
                 ->save($this->image);
         }
         
