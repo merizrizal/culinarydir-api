fca92b1d1f6907c18428834a4e1e23099e939feb
AxelRj13 <axel.raharja1@gmail.com>
2019-04-03 17:53:17 +0700

    Penambahan API untuk apps driver(progress)

--- controllers/v1/OrderController.php
+++ controllers/v1/OrderController.php
@@ -2,10 +2,11 @@
 
 namespace api\controllers\v1;
 
+use frontend\components\AddressType;
 use Yii;
 use yii\filters\VerbFilter;
 use core\models\TransactionSession;
-use core\models\TransactionItem;
+use yii\web\UploadedFile;
 
 class OrderController extends \yii\rest\Controller
 {
@@ -22,7 +23,12 @@ class OrderController extends \yii\rest\Controller
                     'class' => VerbFilter::className(),
                     'actions' => [
                         'get-order-header' => ['POST'],
-                        'get-order-detail' => ['POST']
+                        'get-order-detail' => ['POST'],
+                        'upload-receipt' => ['POST'],
+                        'update-status-order' => ['POST'],
+                        'confirm-price' => ['POST'],
+                        'calculate-delivery-fee' => ['POST'],
+                        'update-driver-position' => ['POST']
                     ],
                 ],
             ]);
@@ -32,12 +38,18 @@ class OrderController extends \yii\rest\Controller
     {
         $post = Yii::$app->request->post();
         
-        $result = [];
-        
         $modelTransactionSession = TransactionSession::find()
+            ->joinWith([
+                'business',
+                'business.businessLocation',
+                'userOrdered',
+                'userOrdered.userPerson.person'
+            ])
             ->andFilterWhere(['ilike', 'order_id', $post['order_id'] . '_'])
             ->andFilterWhere(['driver_username' => $post['driver_username']])
             ->asArray()->all();
+        
+        $result = [];
         
         if (!empty($modelTransactionSession)) {
             
@@ -47,8 +59,21 @@ class OrderController extends \yii\rest\Controller
                 
                 $result[$i] = [];
                 
-                $result[$i]['user_ordered'] = $dataTransactionSession['user_ordered'];
-                $result[$i]['business_id'] = $dataTransactionSession['business_id'];
+                $result[$i]['customer_name'] = $dataTransactionSession['userOrdered']['full_name'];
+                $result[$i]['customer_username'] = $dataTransactionSession['userOrdered']['username'];
+                $result[$i]['customer_phone'] = $dataTransactionSession['userOrdered']['userPerson']['person']['phone'];
+                $result[$i]['customer_address'] = $dataTransactionSession['userOrdered']['userPerson']['person']['address'];
+                
+                $result[$i]['business_name'] = $dataTransactionSession['business']['name'];
+                $result[$i]['business_phone'] = $dataTransactionSession['business']['phone1'];
+                $result[$i]['business_coordinate'] = $dataTransactionSession['business']['businessLocation']['coordinate'];
+                $result[$i]['business_address'] = 
+                    AddressType::widget([
+                        'businessLocation' => $dataTransactionSession['business']['businessLocation'],
+                        'showDetail' => false
+                ]);
+                
+                $result[$i]['order_id'] = $dataTransactionSession['order_id'];
                 $result[$i]['note'] = $dataTransactionSession['note'];
                 $result[$i]['total_price'] = $dataTransactionSession['total_price'];
                 $result[$i]['total_amount'] = $dataTransactionSession['total_amount'];
@@ -69,8 +94,6 @@ class OrderController extends \yii\rest\Controller
     {
         $post = Yii::$app->request->post();
         
-        $result = [];
-        
         $modelTransactionSession = TransactionSession::find()
             ->joinWith([
                 'transactionItems',
@@ -79,19 +102,23 @@ class OrderController extends \yii\rest\Controller
             ->andWhere(['ilike', 'order_id', $post['order_id'] . '_'])
             ->asArray()->one();
         
+        $result = [];
+        
         if (!empty($modelTransactionSession)) {
-            
-            $result['success'] = true;
             
             foreach ($modelTransactionSession['transactionItems'] as $i => $dataTransactionItem) {
                 
                 $result[$i] = [];
                 
                 $result[$i]['menu'] = $dataTransactionItem['businessProduct']['name'];
-                $result[$i]['note'] = $dataTransactionItem['note'];
                 $result[$i]['price'] = $dataTransactionItem['price'];
                 $result[$i]['amount'] = $dataTransactionItem['amount'];
+                $result[$i]['note'] = $dataTransactionItem['note'];
             }
+            
+            $result['total_price'] = $modelTransactionSession['total_price'];
+            $result['total_amount'] = $modelTransactionSession['total_amount'];
+            $result['success'] = true;
         } else {
             
             $result['success'] = false;
@@ -99,5 +126,117 @@ class OrderController extends \yii\rest\Controller
         }
             
         return $result;
+    }
+    
+    public function actionUploadReceipt()
+    {
+        $post = Yii::$app->request->post();
+        
+        $transaction = Yii::$app->db->beginTransaction();
+        $flag = false;
+        
+        $modelTransactionSession = TransactionSession::find()
+            ->andWhere(['ilike', 'order_id', $post['order_id'] . '_'])
+            ->one();
+            
+        $file = UploadedFile::getInstanceByName('image');
+        
+        $result = [];
+        
+        $result['success'] = false;
+        $result['message'] = 'Gagal Upload Resi';
+        
+        if (!empty($modelTransactionSession) && $file) {
+            
+            $fileName = 'AD-' . $post['order_id'] . '.' . $file->extension;
+            
+            if (($flag = $file->saveAs(Yii::getAlias('@uploads') . '/img/transaction_session/' . $fileName))) {
+                
+                $modelTransactionSession->image = $fileName;
+                $flag = $modelTransactionSession->save();
+                
+                if ($flag) {
+                    
+                    $result['success'] = true;
+                    $result['message'] = 'Upload Resi Berhasil';
+                    
+                    $transaction->commit();
+                } else {
+                    
+                    $result['error'] = $modelTransactionSession->getErrors();
+                    
+                    $transaction->rollBack();
+                }
+            }
+        } else {
+            
+            $transaction->rollBack();
+        }
+        
+        return $result;
+    }
+    
+    public function actionConfirmPrice()
+    {
+        $post = Yii::$app->request->post();
+        
+        $modelTransactionSession = TransactionSession::find()
+            ->andWhere(['ilike', 'order_id', $post['order_id'] . '_'])
+            ->one();
+        
+        $result = [];
+        
+        $result['success'] = false;
+        
+        if (!empty($modelTransactionSession)) {
+            
+            $modelTransactionSession->total_price = !empty($post['total_price']) ? $post['total_price'] : $modelTransactionSession->total_price;
+            
+            if ($modelTransactionSession->save()) {
+                
+                $result['success'] = true;
+                $result['message'] = 'Konfirmasi Harga Berhasil';
+            } else {
+                
+                $result['message'] = 'Konfirmasi Harga Gagal';
+                $result['error'] = $modelTransactionSession->getErrors();
+            }
+        } else {
+            
+            $result['message'] = 'Order ID tidak ditemukan';
+        }
+        
+        return $result;
+    }
+    
+    public function actionCalculateDeliveryFee()
+    {
+        
+    }
+    
+    public function actionUpdateDriverPosition()
+    {
+        
+    }
+    
+    private function actionUpdateStatusOrder($orderId, $status)
+    {
+        $modelTransactionSession = TransactionSession::find()
+            ->andWhere(['ilike', 'order_id', $orderId . '_'])
+            ->one();
+            
+        $success = false;
+        
+        if (!empty($modelTransactionSession)) {
+            
+            $modelTransactionSession->order_status = $status;
+            
+            if ($modelTransactionSession->save()) {
+                
+                $success = true;
+            }
+        }
+        
+        return $success;
     }
 }
