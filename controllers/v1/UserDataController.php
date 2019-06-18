dc140ff4dfe5be7bf103f9bb755b2174393442b5
Axel Raharja <axel.raharja1@gmail.com>
2019-06-18 17:59:49 +0700

    Penambahan api untuk get order history

---
+++ controllers/v1/UserDataController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace api\controllers\v1;
+
+use yii\filters\VerbFilter;
+use yii\data\ActiveDataProvider;
+use core\models\TransactionSession;
+
+class UserDataController extends \yii\rest\Controller
+{
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+        'linksEnvelope' => 'links',
+        'metaEnvelope' => 'meta',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'order-history-list' => ['GET'],
+                    ],
+                ],
+            ]);
+    }
+
+    public function actionOrderHistoryList()
+    {
+        $userId = \Yii::$app->request->get('user_id');
+
+        $modelTransactionSession = TransactionSession::find()
+            ->select([
+                'transaction_session.id', 'transaction_session.status', 'transaction_session.created_at', 'transaction_session.total_price',
+                'business.id as business_id', 'business.unique_name as business_unique_name', 'business.name as business_name',
+                'business_location.address as business_address', 'business_image.image as business_image'
+            ])
+            ->joinWith([
+                'business' => function ($query) {
+
+                    $query->select(['business.id']);
+                },
+                'business.businessImages' => function ($query) {
+
+                    $query->select(['business_image.business_id'])
+                        ->andOnCondition(['business_image.type' => 'Profile'])
+                        ->andOnCondition(['business_image.is_primary' => true]);
+                },
+                'business.businessLocation' => function ($query) {
+
+                    $query->select(['business_location.business_id']);
+                }
+            ])
+            ->andWhere(['transaction_session.user_ordered' => $userId])
+            ->orderBy(['transaction_session.created_at' => SORT_DESC])
+            ->distinct()
+            ->asArray();
+
+        $provider = new ActiveDataProvider([
+            'query' => $modelTransactionSession,
+        ]);
+
+        return $provider;
+    }
+}
