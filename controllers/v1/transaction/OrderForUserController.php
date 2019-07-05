48c81cfabaa5f348df4df1ab978a924a32986c08
AxelRj13 <axel.raharja1@gmail.com>
2019-07-05 14:23:32 +0700

    Revert "Penyesuaian layout order history + perbaikan"
    This reverts commit adfe1007cc709d9feca2f096ffb309914a45b125.

--- controllers/v1/transaction/OrderForUserController.php
+++ controllers/v1/transaction/OrderForUserController.php
@@ -433,9 +433,9 @@ class OrderForUserController extends \yii\rest\Controller
         $post = \Yii::$app->request->post();
         
         $modelTransactionSession = TransactionSession::find()
-            ->andWhere(['user_ordered' => $post['user_id']])
-            ->andWhere(['status' => 'Open'])
-            ->asArray()->one();
+        ->andWhere(['user_ordered' => $post['user_id']])
+        ->andWhere(['status' => 'Open'])
+        ->asArray()->one();
         
         if (!empty($modelTransactionSession)) {
             
@@ -455,9 +455,9 @@ class OrderForUserController extends \yii\rest\Controller
             $flag = false;
             
             $oldModelTransaction = TransactionSession::find()
-                ->joinWith(['transactionItems'])
-                ->andWhere(['transaction_session.id' => $post['id']])
-                ->one();
+            ->joinWith(['transactionItems'])
+            ->andWhere(['transaction_session.id' => $post['id']])
+            ->one();
             
             $totalPrice = 0;
             
@@ -521,35 +521,25 @@ class OrderForUserController extends \yii\rest\Controller
         
         if (!empty($get['transaction_session_id'])) {
             
-            $modelTransactionSession = TransactionSession::find()
-                ->joinWith([
-                    'transactionSessionDelivery',
-                    'transactionSessionDelivery.driver',
-                    'transactionSessionDelivery.driver.userPerson.person'
-                ])
-                ->andWhere(['transaction_session.id' => $get['transaction_session_id']])
-                ->asArray()->one();
+            $modelTransactionSessionDelivery = TransactionSessionDelivery::find()
+            ->joinWith([
+                'transactionSession',
+                'driver',
+                'driver.userPerson.person'
+            ])
+            ->andWhere(['transaction_session_id' => $get['transaction_session_id']])
+            ->asArray()->one();
             
-            if (!empty($modelTransactionSession)) {
-            
-                $result['success'] = true;
-                $result['status'] = $modelTransactionSession['status'];
-                
-                if (!empty($modelTransactionSession['transactionSessionDelivery'])) {
-                    
-                    $result['delivery_fee'] = $modelTransactionSession['transactionSessionDelivery']['total_delivery_fee'];
-                    $result['total_price'] = $modelTransactionSession['total_price'];
-                    $result['driver_fullname'] = $modelTransactionSession['transactionSessionDelivery']['driver']['full_name'];
-                    $result['driver_photo'] = $modelTransactionSession['transactionSessionDelivery']['driver']['image'];
-                    $result['driver_phone'] = $modelTransactionSession['transactionSessionDelivery']['driver']['userPerson']['person']['phone'];
-                }
-            } else {
-                
-                $result['message'] = 'transaction_session_id tidak ditemukan';
-            }
+            $result['success'] = true;
+            $result['status'] = $modelTransactionSessionDelivery['transactionSession']['status'];
+            $result['delivery_fee'] = $modelTransactionSessionDelivery['total_delivery_fee'];
+            $result['total_price'] = $modelTransactionSessionDelivery['transactionSession']['total_price'];
+            $result['driver_fullname'] = $modelTransactionSessionDelivery['driver']['full_name'];
+            $result['driver_photo'] = $modelTransactionSessionDelivery['driver']['image'];
+            $result['driver_phone'] = $modelTransactionSessionDelivery['driver']['userPerson']['person']['phone'];
         } else {
             
-            $result['message'] = 'transaction_session_id tidak boleh kosong';
+            $result['message'] = 'transaction_session_id tidak ditemukan';
         }
         
         return $result;
